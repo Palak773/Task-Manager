fb4d7ac9092932cf0a37959e240568e9a074d36a
Palak Gupta <guptapalak201204@gmail.com>
2026-03-20 14:15:31 +0530

    Updated frontend API URL to deployed backend

--- frontend-task/src/api/taskApi.js
+++ frontend-task/src/api/taskApi.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api/tasks";
+const API_URL = "https://task-manager-backend-93nr.onrender.com/api/tasks";
 
 export async function getTasks() {
   const res = await fetch(API_URL);
